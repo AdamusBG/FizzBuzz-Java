17a8ad8d314837ad0face34fbc3061374a7d3ccb
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-04-12 11:13:11 +0100

    updated Fizzify to pass tests for Fizzify#convert for converting to Fizz

--- src/main/java/Fizzify.java
+++ src/main/java/Fizzify.java
@@ -1,6 +1,6 @@
 public class Fizzify {
     public String convert(int number) {
-        if (number == 3) {
+        if (number % 3 == 0) {
             return "Fizz";
         }
         return Integer.toString(number);
