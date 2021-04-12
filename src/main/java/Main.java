e4ac71dbe27b8c43b051d2085fe3844d41393fa8
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-04-12 10:51:47 +0100

    initialized Fizzify class and added loop in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Fizzify fizzifier = new Fizzify();
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(fizzifier.convert(i));
+        }
     }
 }
