10cff95767df6f65a0efe366eecf97da1c6177b6
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-04-12 11:09:57 +0100

    updated Fizzify class to pass failing test

--- src/main/java/Fizzify.java
+++ src/main/java/Fizzify.java
@@ -1,5 +1,8 @@
 public class Fizzify {
     public String convert(int number) {
+        if (number == 3) {
+            return "Fizz";
+        }
         return Integer.toString(number);
     }
 }
