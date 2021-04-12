4b5fc7c16d0df863a806b7bbf76361b4a7c7a4fd
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-04-12 12:01:32 +0100

    refactored Fizzify to remove magic numbers

--- src/main/java/Fizzify.java
+++ src/main/java/Fizzify.java
@@ -1,12 +1,21 @@
 public class Fizzify {
+
+    private final int FIZZ_FACTOR;
+    private final int BUZZ_FACTOR;
+
+    public Fizzify() {
+        FIZZ_FACTOR = 3;
+        BUZZ_FACTOR = 5;
+    }
+
     public String convert(int number) {
-        if (number % 3 == 0 && number % 5 ==0) {
+        if (number % FIZZ_FACTOR == 0 && number % BUZZ_FACTOR == 0) {
             return "FizzBuzz";
         }
-        if (number % 3 == 0) {
+        if (number % FIZZ_FACTOR == 0) {
             return "Fizz";
         }
-        if (number % 5 == 0) {
+        if (number % BUZZ_FACTOR == 0) {
             return "Buzz";
         }
         return Integer.toString(number);
